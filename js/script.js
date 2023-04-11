050065659f5d11051751c7c49bf09e44026fb924
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-11 17:12:36 +0000

    math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file generates the area of a triangle
+"use strict"
+function calculate() {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+  const round = 4 / 3
+
+  // process
+  const volume = round * Math.PI * radius ** 3
+
+  // output
+  document.getElementById("volume").innerHTML = "Volume is: " + volume.toFixed(2) + " cm³"
+}
